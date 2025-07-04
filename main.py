2d6afeb62703ec256f9a97ee60b965974d7da5cb
Celestialic <Schevtsov.german@yandex.ru>
2025-07-05 02:43:33 +0500

    --------

--- main.py
+++ main.py
@@ -2,43 +2,41 @@ import pyautogui
 import time
 
 
-t = time
-g = pyautogui
+class AutoClicker:
+    def __init__(self):
+        self.x_enterline = (387, 209)
+        self.z_enterline = (395, 255)
+        self.btn_setcoords = (384, 347)
+        self.btn_launch = (395, 392)
+
+    def enter_coord(self, coord, position):
+        pyautogui.moveTo(position[0], position[1])
+        pyautogui.click()
+        pyautogui.write(coord)
+
+    def execute_commands(self):
+        pyautogui.moveTo(self.btn_setcoords[0], self.btn_setcoords[1])
+        pyautogui.click()
+        pyautogui.moveTo(self.btn_launch[0], self.btn_launch[1])
+        pyautogui.click()
+
+    def run(self):
+        while True:
+            coords = input('COORDS - XZ: ').strip()
+            if not coords:
+                continue
+
+            coords_split = coords.split()
+            if len(coords_split) != 2:
+                print("Please enter two coordinates separated by a space.")
+                continue
+
+            time.sleep(3)
+            self.enter_coord(coords_split[0], self.x_enterline)
+            self.enter_coord(coords_split[1], self.z_enterline)
+            self.execute_commands()
 
 
-x_enterline = [387, 209]
-z_enterline = [395, 255]
-btn_setcoords = [384, 347]
-btn_launch = [395, 392]
-
-
-def dx_enterline():
-    g.moveTo(x_enterline[0], x_enterline[1])
-    g.leftClick()
-    g.typewrite(coords_split[0])
-
-def dz_enterline():
-    g.moveTo(z_enterline[0], z_enterline[1])
-    g.leftClick()
-    g.typewrite(coords_split[1])
-
-
-def dstart_up():
-    g.moveTo(btn_setcoords[0], btn_setcoords[1])
-    g.leftClick()
-    g.moveTo(btn_launch[0], btn_launch[1])
-    g.leftClick()
-
-
-while True:
-    coords = str(input('COORDS - XZ: '))
-    coords_split = coords.split()
-
-    t.sleep(3)
-
-    dx_enterline()
-    dz_enterline()
-    dstart_up()
-
-    print("Debug:")
-    print(coords_split, x_enterline, z_enterline)
+if __name__ == "__main__":
+    autoclicker = AutoClicker()
+    autoclicker.run()
